d3506536264b45c4b3b03b562112bcb0383929a0
Daniel Sudzilouski <daniel@sudzilouski.com>
2020-04-17 11:29:21 -0700

    actions v6

---
+++ .github/workflows/install_linux.sh
@@ -0,0 +1,18 @@
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+npm install 
+
+# install stable watchman from source
+git clone https://github.com/facebook/watchman.git -b v4.9.0 --depth 1
+cd watchman
+./autogen.sh
+./configure
+make
+sudo make install
+cd ..
+
+# Build app
+npx react-native run-android
---
+++ .github/workflows/install_mac.sh
@@ -0,0 +1,9 @@
+npm install
+
+# Build dependencies
+brew update
+brew install watchman
+sudo gem install cocoapods
+cd ios && pods install && cd ...
+
+npx react-native run-ios
