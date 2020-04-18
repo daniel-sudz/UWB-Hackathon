bc77cb21bee4ef07a34f8a8f80fecd6b19c287c5
Jason Zhang <zjjc123@hotmail.com>
2020-04-18 13:32:56 -0700

    Allowing CORS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,5 +11,11 @@ import parse from './handlers/parse'
 app.use(bodyParser.json())
 app.use('/', parse)
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 exports.api = functions.https.onRequest(app)
